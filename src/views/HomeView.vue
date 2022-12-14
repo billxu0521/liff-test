319c5dcdaa6442e944bbf3518aab06e014e1a580
billxu <billxu0521@gmail.com>
2022-12-14 15:37:21 +0800

    add

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { onMounted } from "vue";
+
+onMounted(async () => {
+    try {
+      await liff.init({ liffId: "1657732539-zn43wJXG" }); // Use own liffId
+      if (!liff.isLoggedIn())
+        liff.login({ redirectUri: window.location.href });
+    } catch (err) {
+      console.log(`liff.state init error ${err}`);
+    }
+  })
+</script>
+
+<template>
+  <div>HELLo</div>
+</template>
