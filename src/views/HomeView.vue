e2eea195e5222ada5e65631896ad532920987842
billxu <billxu0521@gmail.com>
2022-12-14 17:17:10 +0800

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,6 @@
 import { onMounted } from "vue";
 import liff from "@line/liff";
 
-const language, version, isInClient, isLoggedIn, os, lineVersion;
-
 onMounted(async () => {
     liff
     .init({
@@ -11,12 +9,12 @@ onMounted(async () => {
     })
     .then(() => {
       if (!liff.isLoggedIn()) liff.login({ redirectUri: window.location.href });
-      language = liff.getLanguage(); // String。引用 LIFF SDK 的頁面，頁面中的 lang 值
-      version = liff.getVersion(); // String。LIFF SDK 的版本
-      isInClient = liff.isInClient(); // Boolean。回傳是否由 LINE App 存取
-      isLoggedIn = liff.isLoggedIn(); // Boolean。使用者是否登入 LINE 帳號。true 時，可呼叫需要 Access Token 的 API
-      os = liff.getOS(); // String。回傳使用者作業系統：ios、android、web
-      lineVersion = liff.getLineVersion(); // 使用者的 LINE 版本
+      const language = liff.getLanguage(); // String。引用 LIFF SDK 的頁面，頁面中的 lang 值
+      const version = liff.getVersion(); // String。LIFF SDK 的版本
+      const isInClient = liff.isInClient(); // Boolean。回傳是否由 LINE App 存取
+      const isLoggedIn = liff.isLoggedIn(); // Boolean。使用者是否登入 LINE 帳號。true 時，可呼叫需要 Access Token 的 API
+      const os = liff.getOS(); // String。回傳使用者作業系統：ios、android、web
+      const lineVersion = liff.getLineVersion(); // 使用者的 LINE 版本
     })
     .catch((err) => {
       console.log(err.code, err.message);
