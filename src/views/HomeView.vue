90bb07f51fa66ea3239ec105e45590156066692b
billxu <billxu0521@gmail.com>
2022-12-14 16:44:11 +0800

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,37 @@
 <script setup>
 import { onMounted } from "vue";
+import liff from "@line/liff";
+
+const language, version, isInClient, isLoggedIn, os, lineVersion;
 
 onMounted(async () => {
-    try {
-      await liff.init({ liffId: "1657732539-zn43wJXG" }); // Use own liffId
-      if (!liff.isLoggedIn())
-        liff.login({ redirectUri: window.location.href });
-    } catch (err) {
-      console.log(`liff.state init error ${err}`);
-    }
+    liff
+    .init({
+      liffId: "1657732539-zn43wJXG", // Use own liffId
+    })
+    .then(() => {
+      if (!liff.isLoggedIn()) liff.login({ redirectUri: window.location.href });
+      language = liff.getLanguage(); // String。引用 LIFF SDK 的頁面，頁面中的 lang 值
+      version = liff.getVersion(); // String。LIFF SDK 的版本
+      isInClient = liff.isInClient(); // Boolean。回傳是否由 LINE App 存取
+      isLoggedIn = liff.isLoggedIn(); // Boolean。使用者是否登入 LINE 帳號。true 時，可呼叫需要 Access Token 的 API
+      os = liff.getOS(); // String。回傳使用者作業系統：ios、android、web
+      lineVersion = liff.getLineVersion(); // 使用者的 LINE 版本
+    })
+    .catch((err) => {
+      console.log(err.code, err.message);
+    });
   })
 </script>
 
 <template>
-  <div>HELLo</div>
+  <div>
+    <p>{{language}}</p>
+    <p>{{version}}</p>
+    <p>{{isInClient}}</p>
+    <p>{{isLoggedIn}}</p>
+    <p>{{os}}</p>
+    <p>{{lineVersion}}</p>
+
+  </div>
 </template>
